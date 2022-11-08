8cc9d1a66e4720d43428238dde258d68d2b32648
abrahon <mdabrahon926@gmail.com>
2022-11-08 12:30:08 +0600

    added question 2

--- src/Pages/Home/Blog/Blog.js
+++ src/Pages/Home/Blog/Blog.js
@@ -9,7 +9,11 @@ const Blog = () => {
            
             </div>
 
-            
+            <div>
+                <h1 className='text-3xl font-bold text-orange-600 m-5'>What is JWT, and how does it work?</h1>
+                <p>JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.<br/> JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.</p>
+            </div>
+           
         </div>
     )
        
