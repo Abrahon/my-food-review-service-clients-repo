2ebd88f22b77c08a9827f0abf0ad0c9c18597e5a
abrahon <mdabrahon926@gmail.com>
2022-11-08 12:31:15 +0600

    added question 3

--- src/Pages/Home/Blog/Blog.js
+++ src/Pages/Home/Blog/Blog.js
@@ -13,6 +13,17 @@ const Blog = () => {
                 <h1 className='text-3xl font-bold text-orange-600 m-5'>What is JWT, and how does it work?</h1>
                 <p>JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.<br/> JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.</p>
             </div>
+
+            <div>
+                <h1 className='text-3xl font-bold text-orange-600 m-5'>What is the difference between javascript and NodeJS?</h1>
+                <p>JavaScript is a proper high-level programming language used to create web scripts whereas Node.js is a run time environment built on google’s v8 engine.
+                     JavaScript is executed in the browser whereas using Node.js gives us the ability to execute JavaScript outside the browser.
+                      JavaScript can manipulate DOM or add HTML within whereas Node.js doesn’t have the capability to add HTML.
+                       JavaScript is mainly used to create front end web applications or develop client-side whereas Node.js is used on the back end development that is server-side development.
+                        JavaScript follows the standard of JavaScript when writing programs whereas Node.js is written in C++ while using the v8 engine, it runs JavaScript outside the browser.
+                 JavaScript requires any running environment as it can execute on any engine such as Firefox’s spider monkey, v8 engine of google chrome, JavaScript core of Safari whereas Node.js runs only on the v8 engine of google chrome</p>
+            </div>
+            
            
         </div>
     )
